3e210a6e0cd78f4aee9455abbffcc92c67a1a62b
hirano-ta <hirano-ta@ibjapan.jp>
2025-05-29 15:07:08 +0900

    add image input feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update ]
+  allow_unauthenticated_access only: %i[ index show ]
 
   def index
     @products = Product.all
@@ -38,7 +39,7 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description, :featured_image ])
     end
 end
 
